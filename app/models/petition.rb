a86981a700615de36ce29bc9033cf780900a5c56
David O' Rojo <david.orojo@gmail.com>
2016-11-08 15:06:48 -0600

    MIFMN-58 Set Petition :private_fragment, :public_fragment, and :callback_token
    The callback_token is obtained nicely with Rails features. The public and private fragments for the public and private URLs are generated uniquely before the creation of the record.
    This way, these can not be changed on information updates to the petition.

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,2 +1,36 @@
 class Petition < ApplicationRecord
+  before_create :set_private_url_fragment
+  before_create :set_public_url_fragment
+  has_secure_token :callback_token
+
+  private
+
+    def clean_title
+      I18n.transliterate(self.title).split.map { |w| w.gsub(/\W+/, '') }.join(' ')
+    end
+
+    def encode_title
+      clean_title.tr(' ', '-').slice(0,49).chomp('-').downcase
+    end
+
+    def obfuscate_title
+      clean_title.tr(' ', '').slice(0,50).chars.shuffle.join
+    end
+
+    def build_private_fragment
+      "#{Time.now.to_i}#{obfuscate_title}"
+    end
+
+    def build_public_fragment
+      "#{Time.now.to_i}-#{encode_title}"
+    end
+
+    def set_private_url_fragment
+      self.private_fragment = build_private_fragment
+    end
+
+    def set_public_url_fragment
+      self.public_fragment = build_public_fragment
+    end
+
 end
