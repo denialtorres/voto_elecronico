7a94cf0e26e1949ad0eaec05fa5b21e1a3a1090e
David O' Rojo <david.orojo@gmail.com>
2016-11-09 14:21:48 -0600

    MIFMN-62 MIFMN-63 #resolve #time 1h The creator gets access to the public and private links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :petitions
-  get 'petitions/public/:public_fragment', to: 'petitions#show'
-  get 'petitions/private/:private_fragment', to: 'petitions#show'
+  get 'petitions/public/:public_fragment', to: 'petitions#show', as: 'public_petition'
+  get 'petitions/private/:private_fragment', to: 'petitions#show', as: 'private_petition'
   post 'services/mifiel', to: 'services#mifiel_callback'
+
+  root to: 'petitions#index'
 end
